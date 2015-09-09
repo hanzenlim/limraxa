8e5e144186404e900c963c3febc446a43c963adf
hlim <hlim@trulia.com>
2015-09-09 11:17:35 -0700

    add jon and hanzen route

--- routes/dt.js
+++ routes/dt.js
@@ -1,13 +1,20 @@
-exports.findAll = function(req, res) {
+exports.findHanzen = function(req, res) {
     var Firebase = require("firebase");
-    
     var myFirebaseRef = new Firebase("https://limraxa.firebaseio.com/");
 
-    var selfRes = res;
-    myFirebaseRef.child("wow").on("value", function(snapshot) {
+    myFirebaseRef.child("hanzen").on("value", function(snapshot) {
 
-      // res.sendfile('public/baho.html')
       res.send(JSON.stringify(snapshot.val()));
-          // console.log(snapshot.val().toString());
+    });
+};
+
+
+exports.findJon = function(req, res) {
+    var Firebase = require("firebase");
+    var myFirebaseRef = new Firebase("https://limraxa.firebaseio.com/");
+
+    myFirebaseRef.child("jon").on("value", function(snapshot) {
+
+      res.send(JSON.stringify(snapshot.val()));
     });
 };
--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.configure(function () {
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
-app.get('/dt', dt.findAll);
+app.get('/hanzen', dt.findHanzen);
+app.get('/jon', dt.findJon);
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
